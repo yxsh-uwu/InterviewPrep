5900ff762eb512afa6b9124961dc40ba0e53f3c5
Yash <yash27560@gmail.com>
2025-04-08 12:48:35 +0530

    Added Home Page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,22 @@
 import { ReactNode } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const RootLayout = ({children}:{children:ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href='/' className='flex items-center gap-2'>
+          <Image 
+            src='/logo.svg' 
+            alt="Logo" 
+            width={38} 
+            height={32}/>
+          <h2 className='text-primary-100'>Prepwise</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
   )
 }
 
