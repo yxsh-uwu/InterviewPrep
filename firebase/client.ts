60a65cb476d4d39b665ba34a56faa1e34319d33f
Yash <yash27560@gmail.com>
2025-04-14 15:39:35 +0530

    Fixed an API Leak

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABOjsrtgiqhs1-mafJ0Nsy1dhgJcgO42o",
-  authDomain: "prepwise-56f1e.firebaseapp.com",
-  projectId: "prepwise-56f1e",
-  storageBucket: "prepwise-56f1e.firebasestorage.app",
-  messagingSenderId: "894116581220",
-  appId: "1:894116581220:web:a76209269c27266365f471",
-  measurementId: "G-RR7KYMXKCF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 
--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,4 +1,10 @@
+'use server';
+
+import { feedbackSchema } from "@/constants";
 import { db } from "@/firebase/admin";
+import { google } from "@ai-sdk/google";
+import { generateObject } from "ai";
+
 export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
     const interviews = await db
         .collection('interviews')
@@ -10,6 +16,7 @@ export async function getInterviewsByUserId(userId: string): Promise<Interview[]
         ...doc.data()
     })) as Interview[]
 }
+
 export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
     const { userId, limit = 20 } = params;
 
@@ -25,10 +32,58 @@ export async function getLatestInterviews(params: GetLatestInterviewsParams): Pr
         ...doc.data()
     })) as Interview[]
 }
+
 export async function getInterviewById(id: string): Promise<Interview | null> {
     const interview = await db
         .collection('interviews')
         .doc(id)
         .get();
     return interview.data() as Interview | null;
+}
+
+export async function createFeedback(params: CreateFeedbackParams) {
+    const { interviewId, userId, transcript } = params;
+
+    try {
+        const formattedTranscript = transcript
+            .map((sentence: { role: string; content: string; }) => (
+                `-${sentence.role}:${sentence.content}\n`
+            )).join('');
+        const { object: {totalScore, categoryScores, strengths, areasForImprovement, finalAssessment} } = await generateObject({
+            model: google('gemini-2.0-flash-001', {
+                structuredOutputs: false,
+            }),
+            schema: feedbackSchema,
+            prompt: `
+                You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+                Transcript:
+                ${formattedTranscript}
+
+                Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+                - **Communication Skills**: Clarity, articulation, structured responses.
+                - **Technical Knowledge**: Understanding of key concepts for the role.
+                - **Problem-Solving**: Ability to analyze problems and propose solutions.
+                - **Cultural & Role Fit**: Alignment with company values and job role.
+                - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+                `,
+            system:
+                "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
+        });
+        const feedback=await db.collection('feedback').add({
+            interviewId,
+            userId,
+            totalScore,
+            categoryScores,
+            strengths,
+            areasForImprovement,
+            finalAssessment,
+            createdAt: new Date().toISOString()
+        })
+        return{
+            success: true,
+            feedbackId: feedback.id
+        }
+    } catch (e) {
+        console.log('Error Saving Feedback', e)
+    }
 }
